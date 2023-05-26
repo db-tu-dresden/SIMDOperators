73c320fad9bdf5877192519efcae712d59f1c5e9
Eric Mier <eric.mier@tu-dresden.de>
2023-05-26 13:40:51 +0200

    Restructured project.
    Decreased complexity of folder structure

--- src/utils/types.hpp
+++ src/utils/types.hpp
@@ -33,7 +33,7 @@
 #include <cstdint>
 #include <concepts>
 
-#include <tvlintrin.hpp>
+#include <tslintrin.hpp>
 
 namespace tuddbs{
 //alias
@@ -71,13 +71,13 @@ concept DataSinkType = DataProviderType<T> &&
 //Helper Structs
 //todo: This should be in the TVL!!!
 namespace details {
-using namespace tvl;
+using namespace tsl;
 template<VectorProcessingStyle Vec, Arithmetic... Ts, std::size_t... I>
 auto broadcast_from_tuple_impl(std::tuple<Ts...> const & tup, std::index_sequence<I...>) {
-  return std::make_tuple(tvl::set1<Vec>(std::get<I>(tup))...);
+  return std::make_tuple(tsl::set1<Vec>(std::get<I>(tup))...);
 }
 }
-using namespace tvl;
+using namespace tsl;
 template<VectorProcessingStyle Vec, Arithmetic... Ts>
 constexpr auto broadcast_from_tuple(std::tuple<Ts...> tup) {
   static_assert(all_of_specific_type<typename Vec::base_type, std::tuple<Ts...>>::value, "Parameters has to be of same type as the specified Vector type.");
