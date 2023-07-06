77a7f538593989832fef869dc43211d113145f67
Eric Mier <eric.mier@tu-dresden.de>
2023-07-06 14:02:20 +0200

    Added update of submodules to clean

--- clean.sh
+++ clean.sh
@@ -6,6 +6,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Remove all the files in the build directory
 rm -rf $DIR/build
 
+# Prompt if submodules should be updated
+read -p "Update submodules? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git submodule update --init --recursive
+else
+    echo "Not updating submodules"
+fi
+
+
 if [ $? -eq 0 ]; then
     echo "Cleaned build directory"
 else
