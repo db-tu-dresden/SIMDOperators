4c0d388e71360930c2b490ff09c2c9ad08f8ed66
Tom Schwarzburg <tom.schwarzburg@mailbox.tu-dresden.de>
2023-08-04 09:33:18 +0200

    Rewrite select wrapper.

--- src/SIMDOperators/wrappers/DAPHNE/select.hpp
+++ src/SIMDOperators/wrappers/DAPHNE/select.hpp
@@ -43,49 +43,7 @@ namespace tuddbs{
 
             DBTUD_CXX_ATTRIBUTE_FORCE_INLINE
             col_ptr operator()(const_col_ptr column, const base_type& predicate){
-                /// Get the alignment of the column
-                typename AlignmentHelper<ps>::Alignment alignment = AlignmentHelper<ps>::getAlignment(column->getRawDataPtr());
-                size_t alignment_elements;
-                if (column->getPopulationCount() < alignment.getElementsUntilAlignment()) {
-                    alignment_elements = column->getPopulationCount();
-                } else {
-                    alignment_elements = alignment.getElementsUntilAlignment();
-                }
-
-
-                auto result = new Column<base_type>(column->getPopulationCount(), ps::vector_size_B());
-
-                auto result_ptr = result->getRawDataPtr();
-                auto column_ptr = column->getRawDataPtr();
-
-
-                /// Scalar preprocessing
-                size_t pos_count = select<scalar, CompareOperator>::batch::apply( result_ptr, column_ptr, predicate, alignment_elements, 0 );
-                // std::cout << "Scalar preprocessing: " << alignment_elements << " // " << pos_count << std::endl;
-
-                /// Vector processing
-                size_t vector_count = (column->getPopulationCount() - alignment_elements) / ps::vector_element_count();
-                pos_count += select<ps, CompareOperator>::batch::apply( 
-                    (result_ptr + pos_count), 
-                    (column_ptr + alignment_elements), 
-                    predicate, 
-                    vector_count,
-                    alignment_elements
-                );
-                // std::cout << "Vector processing: " << vector_count << " // " << pos_count << std::endl;
-                /// Scalar postprocessing
-                pos_count += select<ps, CompareOperator>::batch::apply( 
-                    result_ptr + pos_count, 
-                    column_ptr + alignment_elements + vector_count * ps::vector_element_count(), 
-                    predicate, 
-                    column->getPopulationCount() - alignment_elements - vector_count * ps::vector_element_count(),
-                    alignment_elements + vector_count * ps::vector_element_count() 
-                );
-                // std::cout << "Scalar postprocessing: " << column->getPopulationCount() - alignment_elements - vector_count * ps::vector_element_count() << " // " << pos_count << std::endl;
-
-                result->setPopulationCount(pos_count);
-
-                return result;
+                return select<ProcessingStyle, CompareOperator>::apply(column, predicate);
             }
 
 
