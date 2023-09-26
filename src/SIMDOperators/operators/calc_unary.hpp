7b5f508214bdb11da3f9d0c6e42fafdc25fcbaa1
Lennard Tucholke <lennardtucholke@gmail.com>
2023-09-26 17:17:57 +0200

    works for mask_size == element_count

--- src/SIMDOperators/operators/calc_unary.hpp
+++ src/SIMDOperators/operators/calc_unary.hpp
@@ -21,6 +21,7 @@
 
 #include "/home/dertuchi/work/TSL/generated_tsl/generator_output/include/tslintrin.hpp"
 #include <cassert>
+#include <bitset>
 
 namespace tuddbs {
     
@@ -48,7 +49,7 @@ namespace tuddbs {
         struct State_bitlist{
             base_t* result_ptr;
             base_t const* data_ptr;
-            mask_t bitlist_ptr;
+            mask_t* bitlist_ptr;
             size_t count;
         };
 
@@ -69,68 +70,106 @@ namespace tuddbs {
         };
 
         void operator()(State_bitlist& myState){
+            State_bitlist backup = myState;
             size_t const element_count = ps::vector_element_count();
             size_t const mask_size = sizeof(mask_t) * 8;
+            const int mask_count = 1 + ((myState.count - 1) / (sizeof(mask_t)*8));
 
             const base_t *end = myState.data_ptr + myState.count;
+            const mask_t *mask_end = myState.bitlist_ptr + mask_count;
 
             if constexpr(element_count < mask_size){
                 // darauf achten, dass zuerst die relevanten bits verwendet werden (Stichwort: packed )
             }else{
+                base_t* data = reinterpret_cast<base_t*>(std::malloc(element_count*sizeof(base_t)));
+                base_t* start = data;
                 while(myState.data_ptr <= (end - element_count)){
-                    base_t* data;
-                    const base_t *start = data;
-                    int mask_count;
-                    while(mask_count < element_count){
+                    data = start;
+                    int mask_pop = 0;
+                    while(mask_pop < element_count){
                         mask_t mask = *myState.bitlist_ptr;
-                        mask_count = tsl::mask_population_count< ps >(mask);
-
-                        // Mask_count is greater than element count -> split mask so data fits again
-                        if(mask_count > element_count){
+                        int old_count = mask_pop;
+                        mask_pop += tsl::mask_population_count< ps >(mask);
+                        std::cout << "mask_pop: " << mask_pop << std::endl;
+                        // If all masks combined can not fill one register, flush and update state
+                        if(myState.bitlist_ptr > mask_end && mask_pop < element_count){
+                            std::cout << "no load possible -> flushing" << std::endl;
+                            flush(backup);
+                            myState = backup;
+                            return;
+                        }
+                        // mask_pop is greater than element count -> split mask so data fits again
+                        if(mask_pop > element_count){
                             // Find index where to split the mask
                             size_t index = 0;
                             mask_t split_mask = 0;
-                            for (size_t i = 0; i < mask_size && index >= mask_count - element_count; i++) {
-                                split_mask |= (mask_t)1 << i;
-                                if ((mask >> i) & 0b1) {
-                                    index++;
+                            for (size_t i = 0; index < mask_size && i < element_count - old_count; index++) {
+                                split_mask |= (mask_t)1 << index;
+                                if ((mask >> index) & 0b1) {
+                                    i++;
                                 }
                             }
                             // Use adapted mask instead to completely fill data into one register
-                            reg_t vec = tsl::loadu< ps >(myState.data_ptr);
-                            vec = tsl::maskz_mov< ps >(mask, tsl::mask_binary_and(mask, split_mask));
-                            tsl::storeu< ps >(data, vec);
+                            mask_t new_mask = tsl::mask_binary_and< ps >(mask, split_mask);
+                            reg_t vec_temp = tsl::loadu< ps >(myState.data_ptr);
 
+                            // std::cout << "data: [";
+                            // for(int i = 0; i < element_count; i++){
+                            //     std::cout << start[i] << ", ";
+                            // }
+                            // std::cout << "]" << std::endl;
+
+                            tsl::compress_store< ps >(new_mask, data, vec_temp);
+
+                            std::bitset<8> b(mask);
+                            std::bitset<8> i(new_mask);
+                            
                             // Replace mask with inverse of adapted mask to get remaining elements and adapt dataptr
-                            mask_t new_mask = tsl::mask_binary_and(mask, tsl::mask_binary_not(split_mask))
-                            *myState.bitlist_ptr = (mask_t)(new_mask >> index);
-                            myState.data_ptr += index;
+                            new_mask = tsl::mask_binary_and< ps >(mask, tsl::mask_binary_not< ps >(split_mask));
+                            std::bitset<8> n(new_mask);
+                            std::cout << "IF mask: " << b << ", mask_split: " << i << ", new_mask: " << n << std::endl;
+                            *myState.bitlist_ptr = new_mask;
                         }
                         // Valid data fits into register with mask
                         else{
-                            reg_t vec = tsl::loadu< ps >(myState.data_ptr);
-                            vec = tsl::maskz_mov< ps >(mask, vec);
-                            tsl::storeu< ps >(data, vec);
-
+                            tsl::compress_store< ps >(mask, data, tsl::loadu< ps >(myState.data_ptr));
                             myState.bitlist_ptr++;
                             myState.data_ptr += element_count;
+
+                            data += tsl::mask_population_count< ps >(mask);
                         }
                     }
                     reg_t vec = tsl::loadu< ps >(start);
                     reg_t result = Operator< ps, tsl::workaround>::apply(vec);
                     tsl::storeu< ps >(myState.result_ptr, result);
                     myState.result_ptr += element_count;
+                    backup = myState;
                 }
+                std::free(start);
             }
         };
 
         static void flush(State& myState){
             const base_t *end = myState.data_ptr + myState.count;
-            reg_t vec;
             while(myState.data_ptr <= end){
                 *myState.result_ptr++ = Operator< tsl::simd<base_t, tsl::scalar>, tsl::workaround>::apply(*myState.data_ptr++);
             }
         };
+
+        static void flush(State_bitlist& myState){
+            const int mask_count = 1 + ((myState.count - 1) / (sizeof(mask_t)*8));
+            const mask_t* end = myState.bitlist_ptr + mask_count;
+
+            while(myState.bitlist_ptr < end){
+                mask_t mask = *myState.bitlist_ptr++;
+                for(size_t i = 0; i < sizeof(mask_t)*8; i++){
+                    if((mask >> i) & 0b1){
+                        *myState.result_ptr++ = Operator< tsl::simd<base_t, tsl::scalar>, tsl::workaround>::apply(*myState.data_ptr);
+                    }
+                    myState.data_ptr++;
+                }
+            }
+        }
   };
 };//namespace tuddbs
 
--- test/calc_unaryTest.cpp
+++ test/calc_unaryTest.cpp
@@ -5,102 +5,122 @@ template< typename ps , template <typename ...> typename Operator>
 bool test_calc_unary(const size_t batch_size, const size_t data_count){
     using base_t = typename ps::base_type;
     using reg_t = typename ps::register_type;
+    using mask_t = typename ps::imask_type;
+    using offset_t = typename ps::offset_base_type;
 
-    base_t* vec = reinterpret_cast<base_t*>(malloc(data_count*sizeof(base_t)));
-    base_t* test_result = reinterpret_cast<base_t*>(malloc(data_count*sizeof(base_t)));
-    base_t* ref_result = reinterpret_cast<base_t*>(malloc(data_count*sizeof(base_t)));
+    size_t mask_count = 1 + ((data_count - 1) / (sizeof(mask_t)*8));
 
-    base_t* temp_vec = vec;
-    base_t* temp_ref = ref_result;
+    base_t* vec_ref = reinterpret_cast<base_t*>(std::malloc(data_count*sizeof(base_t)));
+    base_t* vec_test = reinterpret_cast<base_t*>(std::malloc(data_count*sizeof(base_t)));
+    mask_t* mask_ref = reinterpret_cast<mask_t*>(std::malloc(mask_count*sizeof(mask_t)));
+    mask_t* mask_test = reinterpret_cast<mask_t*>(std::malloc(mask_count*sizeof(mask_t)));
+    base_t* test_result = reinterpret_cast<base_t*>(std::malloc(data_count*sizeof(base_t)));
+    base_t* ref_result = reinterpret_cast<base_t*>(std::malloc(data_count*sizeof(base_t)));
 
     memset(test_result, 0, data_count*sizeof(base_t));
     memset(ref_result, 0, data_count*sizeof(base_t));
 
     // Random number generator
     auto seed = std::time(nullptr);
+    std::cout << "seed:" << seed << "\n----------------------------------------------" << std::endl;
     std::mt19937 rng(seed);
     std::uniform_int_distribution<base_t> dist(1, std::numeric_limits<base_t>::max());
+    std::uniform_int_distribution<base_t> dist_mask(0, std::numeric_limits<mask_t>::max());
 
     // Fill memory with test data
+    base_t* temp_vec = vec_ref;
+    base_t* temp_ref = ref_result;
     for(int i = 0; i < data_count; i++){
         *temp_vec++ = dist(rng);
     }
-    temp_vec = vec;
+    std::memcpy(vec_test, vec_ref, data_count*sizeof(base_t));
 
-    // Ref Value
-    while(temp_vec  <= (vec + data_count)){
-        *temp_ref++ = Operator< tsl::simd<base_t, tsl::scalar>, tsl::workaround>::apply(*temp_vec++);
+    mask_t* temp_mask = mask_ref;
+    for(int i = 0; i < mask_count; i++){
+        *temp_mask++ = dist_mask(rng);
     }
-    temp_vec = vec;
-    temp_ref = ref_result;
+    std::memcpy(mask_test, mask_ref, mask_count*sizeof(mask_t));
+    
+    // Ref Value
+    typename tuddbs::calc_unary<ps, Operator>::State_bitlist state_ref = {.result_ptr = ref_result, .data_ptr = vec_ref,.bitlist_ptr = mask_ref, .count = data_count};
+    tuddbs::calc_unary<ps, Operator>::flush(state_ref);
+
+    // Mal anschauen ob ich das auch so hinbekomme, dann
+    // temp_vec = vec_ref;
+    // base_t* temp_ref = ref_result;
+    // temp_mask = mask_ref;
+    // while(temp_mask < (mask_ref + mask_count)){
+    //     for(size_t i = 0; i < sizeof(mask_t)*8; i++){
+    //         mask_t mask_temp = *temp_mask++;
+    //         if((mask_temp >> i) & 0b1){
+    //             *temp_ref++ = Operator< tsl::simd<base_t, tsl::scalar>, tsl::workaround>::apply(*temp_vec);
+    //         }
+    //         temp_vec++;
+    //     }
+    // }
 
     // Begin test
-    typename tuddbs::calc_unary<ps, Operator>::State state = {.result_ptr = test_result, .data_ptr = vec, .count = data_count};
-    while((state.data_ptr - vec + ps::vector_element_count()) < data_count){
+    std::cout << "Begin Test" << std::endl;
+    typename tuddbs::calc_unary<ps, Operator>::State_bitlist state = {.result_ptr = test_result, .data_ptr = vec_test,.bitlist_ptr = mask_test, .count = batch_size};
+    while((state.data_ptr - vec_test + ps::vector_element_count()) < data_count){
         tuddbs::calc_unary<ps, Operator>{}(state);
 
-        const size_t t = vec + data_count - state.data_ptr;
+        const size_t t = vec_test + data_count - state.data_ptr;
 
         state.count = std::min(batch_size, t);
     }
-    state.count = vec + data_count - state.data_ptr;
+    state.count = vec_test + data_count - state.data_ptr;
     tuddbs::calc_unary<ps, Operator>::flush(state);
 
     //Check if Test is correct
     bool allOk = true;
     temp_vec = test_result;
-    while(allOk && temp_ref <= (ref_result + data_count)){
+    temp_ref = ref_result;
+    while(temp_ref < (ref_result + data_count)){
+        //if(*temp_ref != *temp_vec) std::cout << *temp_ref << " == " << *temp_vec << std::endl;
+        //std::cout << *temp_ref << " == " << *temp_vec << std::endl;
         allOk &= (*temp_ref++ == *temp_vec++);
     }
+    std::cout << "Result: " << allOk << std::endl;
 
-    free(vec);
-    free(test_result);
-    free(ref_result);
+    
+    std::free(test_result);
+    std::free(ref_result);
+    std::free(vec_ref);
+    std::free(vec_test);
+    std::free(mask_ref);
+    std::free(mask_test);
     return allOk;
-}
-
-template<typename ps , template <typename ...> typename Operator, typename state_t>
-state_t calc_unary_exec(state_t state, size_t batch_size){
-    typename ps::base_type* vec = state.data_ptr;
-    while((state.data_ptr - vec + ps::vector_element_count()) < state.count){
-        tuddbs::calc_unary<ps, Operator>{}(state);
-
-        const size_t t = vec + state.count - state.data_ptr;
-
-        state.count = std::min(batch_size, t);
-    }
-    state.count = vec + state.count - state.data_ptr;
-    tuddbs::calc_unary<ps, Operator>::flush(state);
 }
 
 int main()
 {
-    const int count = 10000;
+    const int count = 8*2020;
     bool allOk = true;
     std::cout << "Testing calc_unary...\n";
     std::cout.flush();
     // INTEL - AVX2
     {
-        using ps = typename tsl::simd<uint64_t, tsl::avx2>;
-        const size_t batch_size = ps::vector_element_count();
+        using ps = typename tsl::simd<uint32_t, tsl::avx2>;
+        const size_t batch_size = 2 * ps::vector_element_count();
 
         allOk &= test_calc_unary<ps, tsl::functors::inv>(batch_size, count);
     }
     // INTEL - SSE
-    {
-        using ps = typename tsl::simd<uint64_t, tsl::sse>;
-        const size_t batch_size = ps::vector_element_count();
+    // {
+    //     using ps = typename tsl::simd<uint16_t, tsl::sse>;
+    //     const size_t batch_size = ps::vector_element_count();
 
-        allOk &= test_calc_unary<ps, tsl::functors::inv>(batch_size, count);
-    }
+    //     allOk &= test_calc_unary<ps, tsl::functors::inv>(batch_size, count);
+    // }
     // INTEL - SCALAR
-    {
-        using ps = typename tsl::simd<uint64_t, tsl::scalar>;
-        const size_t batch_size = ps::vector_element_count();
+    // {
+    //     using ps = typename tsl::simd<uint64_t, tsl::scalar>;
+    //     const size_t batch_size = ps::vector_element_count();
 
-        allOk &= test_calc_unary<ps, tsl::functors::inv>(batch_size, count);
-    }
+    //     allOk &= test_calc_unary<ps, tsl::functors::inv>(batch_size, count);
+    // }
     
-    std::cout << "Result: " << allOk << std::endl;
+    //std::cout << "Result: " << allOk << std::endl;
     return 0;
 }
